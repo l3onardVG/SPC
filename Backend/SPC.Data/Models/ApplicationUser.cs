7b93d0f3bc840cf7e10b10b86f3fc21a8e0353ef
Leonard Villegas <leonard.villegas@sourcemeridian.com>
2025-03-28 01:46:01 -0500

    Add aditional fields

--- Backend/SPC.Data/Models/ApplicationUser.cs
+++ Backend/SPC.Data/Models/ApplicationUser.cs
@@ -3,4 +3,9 @@ using Microsoft.AspNetCore.Identity;
 namespace SPC.Data.Models;
 
 public class ApplicationUser: IdentityUser
-{}
+{
+    public string DocumentType { get; set; }
+    public string DocumentNumber { get; set; }
+    public bool TermsAceptance { get; set; }
+    public string UserType { get; set; }
+}
